cad2cb63067f1730b1b8a23db7bab618f876ef22
misha <detong>
2025-12-20 07:10:51 +0800

    tmp

--- curls.sh
+++ curls.sh
@@ -6,6 +6,10 @@ curl -X POST "http://localhost:3001/v1/sessions/create" -H "Content-Type: applic
 
  curl -X POST "http://localhost:3001/v1/chat/completions"   -H "session-id: 0f479c2e-fcfd-4d68-b0d7-8d5f9c52fc0f"      -H "Content-Type: application/json"       -d '{"stream":true,
          "model": "claude-3-sonnet", 
-         "messages": [{"role": "user", "content": "嗯嗯 想要飞呀飞却飞呀飞不高"}]
+         "messages": [{"role": "user",  "sequence_id":"2","content": "嗯嗯"}]
       }'
+
+curl -X POST "http://localhost:3001/api/podcast/generate" \
+      -H "Content-Type: application/json" \
+      -d '{"session_id": "0f479c2e-fcfd-4d68-b0d7-8d5f9c52fc0f"}'
 
